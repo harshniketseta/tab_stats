e93afabc019654cfc2d01ca4d887152188193787
Harshniket Seta <harshniketseta@gmail.com>
2019-10-15 03:17:19 +0530

    Adding finishing touches

--- background.js
+++ background.js
@@ -1,18 +1,20 @@
-var key = "tab_stats";
-
-function newTabStats(tabId) {
-  return {
-    id: tabId,
-    openedAt: Date.now()
-  };
-}
-
 var tabStatsService = (function TabStatsService() {
   return {
     key: "tab_stats",
     event_queue: [],
     processing_queue: [],
     timeout: undefined,
+    initTabStats: function() {
+      var tabStats = {};
+      tabStats[this.key] = {};
+      return tabStats;
+    },
+    newTabStats: function(tabId) {
+      return {
+        id: tabId,
+        openedAt: Date.now()
+      };
+    },
     tabCreated: function(tab, tabStats, process) {
       var self = this;
       if (!this.timeout) {
@@ -20,7 +22,7 @@ var tabStatsService = (function TabStatsService() {
           self.processEvents();
         }, 10);
       }
-      console.log("this", this);
+
       if (!process) {
         this.event_queue.push(["tabCreated", tab, tabStats]);
         return;
@@ -31,7 +33,7 @@ var tabStatsService = (function TabStatsService() {
       if (!tabStats[this.key][tab.windowId]) {
         tabStats[this.key][tab.windowId] = {};
       }
-      tabStats[this.key][tab.windowId][tab.id] = newTabStats(tab.id);
+      tabStats[this.key][tab.windowId][tab.id] = this.newTabStats(tab.id);
       chrome.storage.local.set(tabStats);
     },
     tabRemoved: function(tabId, removeInfo, tabStats, process) {
@@ -68,7 +70,6 @@ var tabStatsService = (function TabStatsService() {
         }, 10);
       }
       if (!process) {
-        console.log("this", this);
         this.event_queue.push(["tabUpdated", tabId, tab, tabStats]);
         return;
       }
@@ -79,33 +80,6 @@ var tabStatsService = (function TabStatsService() {
       ) {
         var tabStat = tabStats[this.key][tab.windowId][tabId];
         tabStat.lastLoadedAt = Date.now();
-        chrome.storage.local.set(tabStats);
-      }
-      return undefined;
-    },
-    tabDetached: function(tabId, detachInfo, tabStats, process) {
-      var self = this;
-      if (!this.timeout) {
-        setTimeout(function() {
-          self.processEvents();
-        }, 10);
-      }
-      if (!process) {
-        this.event_queue.push(["tabDetached", tabId, detachInfo, tabStats]);
-        return;
-      }
-      if (tabStats[this.key] && tabStats[this.key][detachInfo.oldWindowId]) {
-        if (!tabStats[this.key][0]) {
-          tabStats[this.key][0] = {};
-        }
-        tabStats[this.key][0] =
-          tabStats[this.key][detachInfo.oldWindowId][tabId];
-        delete tabStats[this.key][detachInfo.oldWindowId][tabId];
-        if (
-          Object.keys(tabStats[this.key][detachInfo.oldWindowId]).length === 0
-        ) {
-          delete tabStats[this.key][detachInfo.oldWindowId];
-        }
         chrome.storage.local.set(tabStats);
       }
       return undefined;
@@ -125,13 +99,12 @@ var tabStatsService = (function TabStatsService() {
         if (!tabStats[this.key][attachInfo.newWindowId]) {
           tabStats[this.key][attachInfo.newWindowId] = {};
         }
-        if (tabStats[this.key][0] && tabStats[this.key][0][tabId]) {
-          tabStats[this.key][attachInfo.newWindowId][tabId] =
-            tabStats[this.key][0][tabId];
-          delete tabStats[this.key][0][tabId];
 
-          if (Object.keys(tabStats[this.key][0]).length === 0) {
-            delete tabStats[this.key][0];
+        for (var windowId in Object.keys(tabStats[this.key])) {
+          if (tabStats[this.key][windowId][tabId]) {
+            tabStats[this.key][attachInfo.newWindowId][tabId] =
+              tabStats[this.key][windowId][tabId];
+            delete tabStats[this.key][windowId][tabId];
           }
         }
 
@@ -140,8 +113,6 @@ var tabStatsService = (function TabStatsService() {
       return undefined;
     },
     processEvents: function() {
-      var self = this;
-
       this.timeout = null;
       while (this.event_queue.length) {
         this.processing_queue.push(this.event_queue.pop());
@@ -149,23 +120,19 @@ var tabStatsService = (function TabStatsService() {
 
       while (this.processing_queue.length) {
         var event = this.processing_queue.shift();
-        console.log("processing event", event);
         var functionName = event.shift();
         if (this[functionName]) {
           event.push(true);
-          this[functionName].apply(self, event);
+          this[functionName].apply(this, event);
         }
       }
-      this.timeout = setTimeout(function() {
-        self.processEvents();
-      }, 10);
     }
   };
 })();
 
 chrome.runtime.onInstalled.addListener(function() {
-  var tabStats = {};
-  tabStats[key] = {};
+  var tabStats = tabStatsService.initTabStats();
+
   chrome.windows.getAll({}, function(windows) {
     windows.map(function(window) {
       chrome.tabs.getAllInWindow(window.id, function(tabs) {
@@ -197,28 +164,8 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   }
 });
 
-chrome.tabs.onDetached.addListener(function(tabId, detachInfo) {
-  chrome.storage.local.get([key], function(tabStats) {
-    tabStatsService.tabDetached(tabId, detachInfo, tabStats);
-  });
-});
-
 chrome.tabs.onAttached.addListener(function(tabId, attachInfo) {
   chrome.storage.local.get([key], function(tabStats) {
     tabStatsService.tabAttached(tabId, attachInfo, tabStats);
   });
-});
-
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-  for (var key in changes) {
-    var storageChange = changes[key];
-    console.log(
-      'Storage key "%s" in namespace "%s" changed. ' +
-        'Old value was "%s", new value is "%s".',
-      key,
-      namespace,
-      JSON.stringify(storageChange.oldValue),
-      JSON.stringify(storageChange.newValue)
-    );
-  }
 });
